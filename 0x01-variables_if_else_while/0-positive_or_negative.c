a723c37879836792a06052587d19a4d3cbf69cc6
Eseosa <eseosaerhabor@gmail.com>
2022-07-01 12:51:50 -0700

    positivity

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
-* main -> assign a random number to the variable n each time it is executed and prints based a condition
+* main -> assign a random number to the variable n each time it is executed and prints out 
+* based a condition
 * Return : always 0
 */
 int main(void)
@@ -13,12 +14,16 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
+{
 	printf("%d is positive\n" , n);
+}
 if (n = 0)
+{
 	printf("%d is zero\n" , n);
+}
 if (n < 0)
 {
-	printf("$d is negative\n" , n);
+	printf("%d is negative\n" , n);
 }
 return (0)
 }
