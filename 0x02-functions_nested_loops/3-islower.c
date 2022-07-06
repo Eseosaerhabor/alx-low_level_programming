7cab6a3d76ec6df42a1c87e107cfee3bc3e66c3a
Eseosa <eseosaerhabor@gmail.com>
2022-07-06 05:45:58 -0700

    functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-* islower -> checks if the character is lowercase
+* islower - checks if the character is lowercase
+* @c : character to check the case
 * Return: returns 1 and 0 depending on condition
 */
 int _islower(int c);
 {
-	return (c > = 'a' && z < == 'z');
+	return (c >= 97 && c <= 122);
 }
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+/**
+ * print_sign - entry point
+ * @n : number to check
+ * Return:0 or 1
+ */
+int print_sign(int n)
+{
+	int numz;
+
+	if (n > o)
+	{
+		numz = 1;
+		_putchar('+');
+	}
+	else if(n == 0)
+	{
+		numz = 0;
+		_putchar('-');
+	}
+	return (numz);
+}
